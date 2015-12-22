0405c04573e72d93b56a380b412c1c8d36c56725
mshossain110 <mshossain110@yahoo.com>
2015-12-22 20:20:53 +0600

    signup page create

--- app/scripts/lib/custom.js
+++ app/scripts/lib/custom.js
@@ -95,14 +95,19 @@ $(function() {
     }
 });
 
-$(document).ready(function(){
-      $(".dropdown").hover(            
+  $(document).ready(function(){
+        var $dropdown= $(".dropdown");
+            $dropdown.children('a').append('<span class="is_dropdown">+</span>');
+      $dropdown.hover(            
         function() {
             $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-            $(this).toggleClass('open');        
+            $(this).toggleClass('open');
+            $(this).find('span.is_dropdown').text('-');    
         },
         function() {
             $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            $(this).toggleClass('open');       
+            $(this).toggleClass('open');
+
+            $(this).find('span.is_dropdown').text('+');       
     });
   });
