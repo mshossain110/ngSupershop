f6927643dc89e099af76399e4c014137ad13bd55
mshossain110 <mshossain110@yahoo.com>
2016-01-26 19:25:26 +0600

    content box design

---
+++ app/images/Angular Mart_files/controller.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngSuperShopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngSuperShopApp
+ */
+angular.module('ngSuperShopApp')
+  .controller('MainCtrl',['$scope',   function ($scope) {
+
+
+
+
+
+   }])
+   .controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope){
+     $rootScope.$broadcast('bodyClass:add', $scope.bodyClass);
+      
+   }])
+  .controller('ShopSingleClt', ['$scope', '$routeParams', '$http','PService',function($scope, $routeParams, $http, PService){
+    var Productid=$routeParams.productID;
+
+
+
+      if(!PService.isEmpty()){
+        $scope.product=PService.getProductByID(Productid);
+      }else{
+          $http.get('data/products.json').success(function(response){
+          PService.setAllProducts(response);
+          $scope.product=PService.getProductByID(Productid);
+        });
+      }
+
+
+  }])
+
+  .controller('amAddtocartClr', ['$scope', function($scope){
+
+  }])
+
+
+  .controller('cardSummeryclrt', ['$scope', 'amCart',  function($scope, amCart){
+    $scope.amCart=amCart;
+  } ])
+
+
+  .controller('amproductclrt', ['$scope', '$http', 'PService',  'amCart', function($scope,$http, PService, amCart){
+
+    if(!PService.isEmpty()){
+      $scope.p=PService;
+      }else{
+          $http.get('data/products.json').success(function(response){
+          PService.setAllProducts(response);
+          $scope.p=PService;
+        });
+      }
+    $scope.amCart=amCart;
+    $scope.query='';
+    $scope.setProductId=function(id){
+      $scope.sproduct=PService.getProductByID(id);
+    };
+  } ]);
